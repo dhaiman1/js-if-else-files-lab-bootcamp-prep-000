a26577fa8862feeee0831ffca1b9d0fbd1ce10c9
David Haiman <dhaiman2@gmail.com>
2018-02-18 23:57:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var height = "74"
 if (name === "susan") {
   console.log("The name is correct")
 } else {
-  console.log("The name is correct")
+  console.log("Expected the name to be Susan")
 }
 
 // Don't worry about this
