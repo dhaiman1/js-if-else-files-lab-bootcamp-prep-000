fdd3e0ff8d115dfc54c38db91f304f2019f8d99a
David Haiman <dhaiman2@gmail.com>
2018-02-18 23:56:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ var name = "Joe"
 var height = "74"
 if (name === "susan") {
   console.log("The name is correct")
-} else 
+} else {
+  console.log("The name is correct")
+}
 
 // Don't worry about this
 module.exports = { name, height
